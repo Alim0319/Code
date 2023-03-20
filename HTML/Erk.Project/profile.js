ef483973e95f95e4d6cb55d7549377061697ada3
AlimErk <alim.kodehode@gmail.com>
2023-03-20 18:33:02 +0100

    h

--- HTML/Erk.Project/profile.js
+++ HTML/Erk.Project/profile.js
@@ -6,26 +6,29 @@ const file = document.querySelector(`#myStayle`);
 const uploadBtn = document.querySelector(`#uploadBtn`);
 
 //if user hover on img div
-imgDiv.addEventListener(`mouseenter`, function () {
-  uploadBtn.style.display = "block";
-});
+// imgDiv.addEventListener(`mouseenter`, function () {
+//   uploadBtn.style.display = "block";
+// });
 
 //if we hover from img div
-imgDiv.addEventListener(`mouseleave`, function () {
-  uploadBtn.style.display = "none";
-});
+// imgDiv.addEventListener(`mouseleave`, function () {
+//   uploadBtn.style.display = "none";
+// });
 // lets work for image showing functionality when we chose an image to upload
 
 // when we choose a foto to upload
 myStayle.addEventListener(`change`, function () {
   //this reders to file
-  const choosedFile = this.myStayles[0];
+  console.log(this.files);
+  const choosedFile = this.files;
   if (choosedFile) {
     const reader = new FileReader();
-    //fileReader is a predefind function of Js
-    reader.addEventListener(`load`, function () {
-      img.setAttribute(`src`, reader.result);
-    });
-    reader.readAsDataURL(choosedFile);
+    // //fileReader is a predefind function of Js
+    // reader.addEventListener(`load`, function () {
+    //   img.setAttribute(`src`, reader.result);
+    // });
+    // reader.readAsDataURL(choosedFile);
+
+    img.src = URL.createObjectURL(this.files[0]);
   }
 });
